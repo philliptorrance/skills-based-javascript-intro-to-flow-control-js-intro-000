4de7c3ecca097460664fd48541113a13feb8bee1
Phillip D Torrance <phillip_torrance@outlook.com>
2020-07-27 11:50:51 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,21 @@
+<<<<<<< HEAD
  function basicTeenager(age) { if ((age) <= 19 && (age) >= 13) {
    return ("You are a teenager!");
+=======
+ function basicTeenager(age) { if ((age) >= 13) {
+   return ("You are a teenager!")
+>>>>>>> c6bc72041455e18f881b1d99a645216cb7f536f2
  }
 
 }
 
+<<<<<<< HEAD
 function teenager(age) { if  ((age) <= 19 && (age) >= 13)  {
   return ('You are a teenager!');
 } else { return ('You are not a teenager')}
+=======
+function teenager(age) { if ((age) )
+>>>>>>> c6bc72041455e18f881b1d99a645216cb7f536f2
 
 }
 
@@ -16,6 +25,13 @@ function ageChecker(age) { if ((age) <= 19 && (age) >= 13) {
 } else { return ('You are a grownup');
 }
 
+<<<<<<< HEAD
+=======
+function ternaryTeenager(age) {
+conditionToTest ?
+valueToBeReturnedIfTrue : "You are a teenager!" 
+valueToBeReturnedIfFalse: "You are not a teenager!"
+>>>>>>> c6bc72041455e18f881b1d99a645216cb7f536f2
 }
 
 function ternaryTeenager(age) { return ((age) <= 19 && (age) >= 13)  ? ('You are a teenager') : ('You are not a teenager'); 
